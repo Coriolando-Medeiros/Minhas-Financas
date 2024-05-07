0b625395b09803de7549c291ea62e28a2209f556
Coriolando Medeiros <coriolando.medeiros@gmail.com>
2024-05-07 04:12:21 -0300

    Fazendo migrações e criando o BD

--- app/settings.py
+++ app/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'category',
-    'transation',
+    'transactions',
 ]
 
 MIDDLEWARE = [
--- transactions/models.py
+++ transactions/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from category.models import Category
 
 
-class Transation(models.Model):
+class Transaction(models.Model):
     value = models.DecimalField(max_digits=10, decimal_places=2)
     type_transaction = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='transactions')
     date = models.DateTimeField(auto_now_add=True)
-    descrition = models.TextField(max_length=300)
-    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='type_category')
+    description = models.TextField(max_length=300)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='transactions_as_category')
 
     class Meta:
         ordering = ['-date']
