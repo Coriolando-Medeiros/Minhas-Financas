eff66c912e27f15b11268b711b631e8f7bbc6faf
Coriolando Medeiros <coriolando.medeiros@gmail.com>
2024-05-07 03:52:46 -0300

    Criando os modelos Category e Transaction

--- category/models.py
+++ category/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    RECEITA = 'receita'
+    DESPESA = 'despesa'
+
+    TIPO_CHOICES = [
+        (RECEITA, 'Receita'),
+        (DESPESA, 'Despesa'),
+    ]
+
+    name = models.TextField(max_length=100)
+    type_category = models.CharField(max_length=20, choices=TIPO_CHOICES)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return str(self.name)
--- transation/models.py
+++ transation/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from category.models import Category
 
-# Create your models here.
+
+class Transation(models.Model):
+    value = models.DecimalField(max_digits=10, decimal_places=2)
+    type_transaction = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='transactions')
+    date = models.DateTimeField(auto_now_add=True)
+    descrition = models.TextField(max_length=300)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='type_category')
+
+    class Meta:
+        ordering = ['-date']
+
+    def __str__(self):
+        return str(self.value)
